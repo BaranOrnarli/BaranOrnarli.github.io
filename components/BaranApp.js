d4d0384413271c81204b915213871067e89cc1f5
BaranOrnarli <bornarli@gmail.com>
2016-08-13 17:16:45 -0400

    modal and animate

--- components/BaranApp.js
+++ components/BaranApp.js
@@ -1,5 +1,5 @@
 
-var InitCtrl = function InitCtrl ($scope, $log) {
+var InitCtrl = function InitCtrl ($scope, $log, $uibModal) {
   $scope.items = [
     'The first choice!',
     'And another choice for you.',
@@ -13,6 +13,7 @@ var InitCtrl = function InitCtrl ($scope, $log) {
   $scope.toggled = function(open) {
     $log.log('Dropdown is now: ', open);
   };
+  $scope.animationsEnabled = true;
 
   $scope.toggleDropdown = function($event) {
     $log.log("Testing toggle: ", open);
@@ -22,9 +23,37 @@ var InitCtrl = function InitCtrl ($scope, $log) {
     $log.log("Finishing toggle: ", open);
   };
 
+  $scope.open = function () {
+
+    var modalInstance = $uibModal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: 'modal.html',
+      controller: 'ModalInstanceCtrl'
+    });
+
+    modalInstance.result.then(function () {
+        $log.info('Modal result at: ' + new Date());
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+
   //$scope.appendToEl = angular.element(document.querySelector('#dropdown-long-content'));
 };
 
-var app = angular.module('BaranOrnarliApp', ['ui.bootstrap']);
+var ModalInstanceCtrl = function ($scope, $uibModalInstance) {
 
-app.controller('InitCtrl', ['$scope', '$log', InitCtrl]);
+  $scope.ok = function () {
+    $uibModalInstance.close();
+  };
+
+  $scope.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+};
+
+var app = angular.module('BaranOrnarliApp', ['ui.bootstrap', 'ngAnimate']);
+
+app.controller('InitCtrl', ['$scope', '$log', '$uibModal', InitCtrl]);
+app.controller('ModalInstanceCtrl', ['$scope', '$uibModalInstance', ModalInstanceCtrl]);
